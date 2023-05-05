47e4c9bea9eb674504a2ecc0d6c13c6eca30ef74
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-05 15:04:50 +0200

    - Import google file

--- SnacktacularUI/App/SnacktacularUIApp.swift
+++ SnacktacularUI/App/SnacktacularUIApp.swift
@@ -5,10 +5,20 @@
 //  Created by Eric on 05/05/2023.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct SnacktacularUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
