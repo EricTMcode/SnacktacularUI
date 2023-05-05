3fea06fda20552abf63cdacb6bd5f3a4cd4d087e
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-05 14:46:49 +0200

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Eric on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
