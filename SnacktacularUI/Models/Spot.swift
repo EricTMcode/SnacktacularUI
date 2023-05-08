924f2b2e6bde0a1540e1e39306ca57bc12a94cd6
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-08 14:52:02 +0200

    - Add map plotting

--- SnacktacularUI/Models/Spot.swift
+++ SnacktacularUI/Models/Spot.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseFirestoreSwift
+import CoreLocation
 
 struct Spot: Identifiable, Codable {
     @DocumentID var id: String?
@@ -14,6 +15,9 @@ struct Spot: Identifiable, Codable {
     var address = ""
     var latitude = 0.0
     var longitude = 0.0
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
     
     var dictionary: [String: Any] {
         return ["name": name, "address": address, "latitude": latitude, "longitude": longitude]
