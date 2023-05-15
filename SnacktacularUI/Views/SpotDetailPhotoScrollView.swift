bf595203e93939c92838d516a4912ae20a42562f
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-15 14:04:18 +0200

    - Create SpotDetailPhotoScrollView with dummy data to show pictures

---
+++ SnacktacularUI/Views/SpotDetailPhotoScrollView.swift
@@ -0,0 +1,45 @@
+//
+//  SpotDetailPhotoScrollView.swift
+//  SnacktacularUI
+//
+//  Created by Eric on 15/05/2023.
+//
+
+import SwiftUI
+
+struct SpotDetailPhotoScrollView: View {
+    struct FakePhoto: Identifiable {
+        let id = UUID().uuidString
+        var imageURLString = "https://firebasestorage.googleapis.com:443/v0/b/snacktacularui-e7088.appspot.com/o/VgWKiDab5D9kkMzIKZFM%2F0917B08C-5C05-425E-A171-56FD3DB7DBA4.jpeg?alt=media&token=d2019db7-5fbf-425e-8dee-0e5fd68d8a2b"
+        
+    }
+    let photos = [FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto(), FakePhoto()]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 4) {
+                ForEach(photos) { photo in
+                    let imageURL = URL(string: photo.imageURLString) ?? URL(string: "")
+                    
+                    AsyncImage(url: imageURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipped()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+            }
+        }
+        .frame(height: 80)
+        .padding(.horizontal, 4)
+    }
+}
+
+struct SpotDetailPhotoScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotDetailPhotoScrollView()
+    }
+}
